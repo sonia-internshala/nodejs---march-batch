16eafe21486d2562373101edbe4161be535c611f
sonia-internshala <sonia.malik@internshala.com>
2025-07-04 21:05:15 +0530

    Add files via upload

--- restaurant.controller.js
+++ restaurant.controller.js
@@ -22,3 +22,52 @@ export function Restaurants(rej,res){
     }
 }
 
+
+export async function createRestaurant(req,res){
+    try{
+        let document=await restaurantModel.create(req.body);
+        return res.status(200).json({"message":`data posted successfully ${document}`})
+    }
+    catch(err){
+        return res.status(404).json({"message":err});
+    }
+}
+
+export async function deleteRestaurant(req,res){
+    try{
+        let id=req.params.id;
+        let deletedDocument=await restaurantModel.findByIdAndDelete(id);
+        return res.status(200).json({"message":`data deleted successfully ${deletedDocument}`})
+    }
+    catch(err){
+        return res.status(404).json({"message":err});
+    }
+}
+
+export async function updateRestaurant(req,res){
+    try{
+        let id=req.params.id;
+        let updates=req.body;
+        let {coupenCode}=req.body;
+        // console.log(coupenCode);
+
+
+        let existingData=await restaurantModel.findById(id);
+        // console.log(existingData);
+        if(!existingData.coupenCode){
+            res.json({"message":`coupenCode doesn't exists`});
+        }
+
+         keys.map((key)=>(
+            (!existingData.key)?
+            res.json({"message":`${key} doesn't exists`})
+            :
+            null
+        ))
+        let updatedDocument=await restaurantModel.findByIdAndUpdate(id,updates,{new:true});
+        return res.status(200).json({"message":`data updated successfully ${updatedDocument} ${coupenCode} ${existingData}`})
+    }
+    catch(err){
+        return res.status(404).json({"message":err});
+    }
+}
