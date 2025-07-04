3aaa50baeafff0dfe9c344a8bac111e5aea1057e
sonia-internshala <sonia.malik@internshala.com>
2025-07-04 21:05:38 +0530

    Add files via upload

--- restaurant.routes.js
+++ restaurant.routes.js
@@ -1,10 +1,13 @@
 
 import Quote from "../Controllers/restaurant.controller.js";
-import { Users , Pizza,Restaurants} from "../Controllers/restaurant.controller.js";
+import { Users , Pizza,Restaurants,createRestaurant,deleteRestaurant,updateRestaurant} from "../Controllers/restaurant.controller.js";
 
 export default function restaurantRoutes(app){
     app.get("/quote",Quote);
     app.get("/users",Users);
     app.get("/pizza",Pizza);
-    app.get("/api/restaurants",Restaurants)
+    app.get("/api/restaurants",Restaurants);
+    app.post("/api/restaurant",createRestaurant);
+    app.delete("/api/restaurant/:id",deleteRestaurant);
+    app.patch("/api/restaurant/:id",updateRestaurant);
 }
